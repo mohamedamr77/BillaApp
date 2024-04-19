0fa51ab3933293a0fe4e329ebc2403c4c325212d
mohamedamr77 <moamr947@gmail.com>
2024-04-20 00:58:18 +0200

    list fav item

--- lib/data_variable/list_fav.dart
+++ lib/data_variable/list_fav.dart
@@ -1,9 +1,5 @@
 import 'package:bella/data_variable/product_model.dart';
 
 List<ProductModel> favouriteItems=[
-  ProductModel(
-    image: "assets/images/Beaf Burger.png",
-    nameProduct: "Beaf Burger",
-    price: "7.5",
-  ),
+
 ];
--- lib/ui/favourate_screen.dart
+++ lib/ui/favourate_screen.dart
@@ -6,19 +6,15 @@ import '../widget/homepagewidget/favoritebody.dart';
 class FavouriteScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-                (context, index) =>FavoriteBody(
-                  image: favouriteItems[index].image,
-                  nameProduct: favouriteItems[index].nameProduct,
-                  price: favouriteItems[index].price,
-                ),
-            childCount: favouriteItems.length,
-          ),
-        ),
-      ],
+    return ListView.separated(
+      shrinkWrap: true,
+      itemBuilder:(context, index) => FavoriteBody(
+      image: favouriteItems[index].image,
+        nameProduct: favouriteItems[index].nameProduct,
+        price: favouriteItems[index].price,
+    ),
+      separatorBuilder: (BuildContext context, int index)=> SizedBox(height: 10,) ,
+      itemCount: favouriteItems.length,
     );
   }
 
