aef9a94ab2eda17233bdc6943cfc0df978b90df9
mohamedamr77 <moamr947@gmail.com>
2024-04-20 01:28:24 +0200

    final project

--- lib/widget/homepagewidget/favoritebody.dart
+++ lib/widget/homepagewidget/favoritebody.dart
@@ -16,65 +16,57 @@ class FavoriteBody extends StatelessWidget{
       );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorApp.bgScaffoldColor,
-      body: Column(
+    return Container(
+      margin: EdgeInsets.all(15),
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height*0.13,
 
-        children: [
-          Container(
-            margin: EdgeInsets.all(15),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height*0.13,
-
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.white,
-            ),
-            child: ListTile(
-              leading: Image.asset(image,
-              width: 80,
-                height: 80,
-              ),
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                      Text("$nameProduct",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontFamily: "Roboto-Regular",
-                        fontWeight: FontWeight.bold,
-                      ),
-                      ),
-                     SizedBox(height: 10,),
-                     Text("$price \$",
-                     style: TextStyle(
-                       color: Color(0xffFF7A00),
-                       fontSize: 16,
-                       fontFamily: "Roboto-Regular",
-                       fontWeight: FontWeight.w700,
-                     ),
-                     ),
-              ]),
-              trailing:Padding(
-                padding: const EdgeInsets.only(
-                  right: 10.0,
-                  top: 9.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      child: ListTile(
+        leading: Image.asset(image,
+          width: 80,
+          height: 80,
+        ),
+        title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("$nameProduct",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: "Roboto-Regular",
+                  fontWeight: FontWeight.bold,
                 ),
-                child: CircleAvatar(
-                  backgroundColor: ColorApp.bgCircleAvatarColor.withOpacity(0.1),
-                  child: Center(
-                    child:  Icon(Icons.favorite,
-                          color:Colors.orange,
-                    ),
-
-                    ),
-                  ),
               ),
+              SizedBox(height: 10,),
+              Text("$price \$",
+                style: TextStyle(
+                  color: Color(0xffFF7A00),
+                  fontSize: 16,
+                  fontFamily: "Roboto-Regular",
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ]),
+        trailing:Padding(
+          padding: const EdgeInsets.only(
+            right: 10.0,
+            top: 9.0,
+          ),
+          child: CircleAvatar(
+            backgroundColor: ColorApp.bgCircleAvatarColor.withOpacity(0.1),
+            child: Center(
+              child:  Icon(Icons.favorite,
+                color:Colors.orange,
+              ),
+
             ),
           ),
-        ],
+        ),
       ),
     );
   }
